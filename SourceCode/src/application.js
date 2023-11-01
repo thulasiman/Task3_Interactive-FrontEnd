dafd93976f7e84545b673a0d9ae4fd85c4eff993
Thulasimani <110888405+thulasiman@users.noreply.github.com>
2023-11-01 14:38:09 +0530

    Add files via upload

---
+++ SourceCode/src/application.js
@@ -0,0 +1,10 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+// import Rails from "./rails-ujs";
+import "./jquery";
+import "jquery_ujs"
+import "./jquery_ui"
+//= require moment
+//= require daterangepicker
+console.log($.ui)
